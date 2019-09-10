7e47134b0b6c15cf84692fbb5e533086e867382e
kevin viglucci <kviglucci@gmail.com>
2019-09-10 01:09:14 -0500

    update ClientConfiguration to correctly constructu RSocket with WebsocketClientTransport

--- src/main/java/com/solidice/springbootrsocketserver/config/ClientConfiguration.java
+++ src/main/java/com/solidice/springbootrsocketserver/config/ClientConfiguration.java
@@ -3,7 +3,9 @@ package com.solidice.springbootrsocketserver.config;
 import io.rsocket.RSocket;
 import io.rsocket.RSocketFactory;
 import io.rsocket.frame.decoder.PayloadDecoder;
+import io.rsocket.transport.ClientTransport;
 import io.rsocket.transport.netty.client.TcpClientTransport;
+import io.rsocket.transport.netty.client.WebsocketClientTransport;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.rsocket.MetadataExtractor;
@@ -11,22 +13,33 @@ import org.springframework.messaging.rsocket.RSocketRequester;
 import org.springframework.messaging.rsocket.RSocketStrategies;
 import org.springframework.util.MimeTypeUtils;
 
+import java.net.URI;
+import java.time.Duration;
+
 @Configuration
 public class ClientConfiguration {
 
     @Bean
     public RSocket rSocket() {
+        URI websocketUri = URI.create("ws://127.0.0.1:8080/ws");
+        WebsocketClientTransport ws = WebsocketClientTransport.create(websocketUri);
         return RSocketFactory
             .connect()
-            .mimeType(MetadataExtractor.ROUTING.toString(), MimeTypeUtils.APPLICATION_JSON_VALUE)
+            .mimeType(
+                MetadataExtractor.ROUTING.toString(),
+                MimeTypeUtils.APPLICATION_JSON_VALUE)
             .frameDecoder(PayloadDecoder.ZERO_COPY)
-            .transport(TcpClientTransport.create(8081))
+            .transport(ws)
             .start()
             .block();
     }
 
     @Bean
     RSocketRequester rSocketRequester(RSocketStrategies rSocketStrategies) {
-        return RSocketRequester.wrap(rSocket(), MimeTypeUtils.APPLICATION_JSON, MimeTypeUtils.APPLICATION_JSON, rSocketStrategies);
+        return RSocketRequester.wrap(
+            rSocket(),
+            MimeTypeUtils.APPLICATION_JSON,
+            MetadataExtractor.ROUTING,
+            rSocketStrategies);
     }
 }
